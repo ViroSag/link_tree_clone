f617612c870f509f62f56b4ad5e71fdda4e1fd8b
ViroSag <116980352+ViroSag@users.noreply.github.com>
2025-09-11 01:19:38 +0500

    routine work

--- app/page.js
+++ app/page.js
@@ -4,12 +4,18 @@ export default function Home() {
   return (
   <main>
     <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
-    <div className="flex  items-center justify-center flex-col ml-[10vw] gap-4">
-<p className="text-yellow-300 font-bold text-5xl">Everything you are. In one, simple link in bio.</p>
-<p className="text-yellow-300 text-xl">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+    <div className="flex justify-center flex-col ml-[10vw] gap-1">
+<p className="text-yellow-300 font-bold text-6xl">Everything you</p>
+<p className="text-yellow-300 font-bold text-6xl">are. In one,</p>
+<p className="text-yellow-300 font-bold text-6xl">simple link in bio.</p>
+<p className="text-yellow-300 text-xl my-4">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+    <div className="input flex gap-2">
+      <input className="bg-white px-2 py-2 focus:outline-green-800 rounded-md" type="text" placeholder="bittr.ee/" />
+      <button className="rounded-full px-4 py-4 bg-pink-300 font-semibold">Claim your BitTree</button>
+    </div>
     </div>
     <div className=" flex justify-center items-center flex-col mr-[10vw]">
-col,2
+<img className="my-32" src="home.png" alt="homepage image" />
     </div>
     </section>
     <section className="bg-red-700 min-h-[100vh]">
